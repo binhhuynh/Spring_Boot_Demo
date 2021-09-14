6322e83aa95e94db3e8e00996cff2782f0c6fc58
Thanh Bình Huỳnh <huynhthanhbinh290@gmail.com>
2021-09-14 12:33:50 +0700

    Complete testing in repo-branch

--- src/main/java/com/htb/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.htb.service;
-
-import com.htb.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class BookService {
-}
--- src/test/java/com/htb/SpringBootDemoApplicationTests.java
+++ src/test/java/com/htb/SpringBootDemoApplicationTests.java
@@ -1,13 +1,52 @@
 package com.htb;
 
+import com.htb.entity.Book;
+import com.htb.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringBootDemoApplicationTests {
+
+	@Autowired
+	private BookRepository bookRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void findAll() {
+		List<Book> books = bookRepository.findAll();
+		books.forEach(System.out::println);
+	}
+
+	@Test
+	void createBook() {
+		Book book = new Book();
+		book.setLanguage("English");
+		book.setPaperback(250);
+		book.setPublisher("O'Really");
+		book.setTitle("Head First Java 2nd");
+		bookRepository.save(book);
+	}
+
+	@Test
+	void updateBook() {
+		Book book = new Book();
+		book.setId(4);
+		book.setLanguage("English");
+		book.setPaperback(250);
+		book.setPublisher("O'Really");
+		book.setTitle("Head First OOP 2nd");
+		bookRepository.save(book);
+	}
+
+	@Test
+	void deleteBook() {
+		bookRepository.deleteById(4);
+	}
 }
